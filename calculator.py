c3e56936b498358f28044c196eaaf92308f8f299
sarika28591 <sarikamulay28@gmail.com>
2018-04-09 14:04:34 +0530

    Fine

---
+++ calculator.py
@@ -0,0 +1,36 @@
+import re
+
+print("Magical calculator")
+print("Type 'quit' to exit\n")
+
+previous = 0
+run = True
+
+def perform_math():
+    global run
+    global previous
+    equation = ""
+    if previous == 0:
+        equation = input("Enter equation:")
+    else:
+        equation = input(str(previous))
+
+
+    if equation == 'quit':
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z,.\()" "]' , '' , equation)
+
+    if previous == 0:
+        previous = eval(equation)
+    else:
+        previous = eval(str(previous) + equation)
+
+
+while run:
+     perform_math()
+
+#Enter equation:5 + 2
+#7 * 2
+#14 / 2
+#7.0
